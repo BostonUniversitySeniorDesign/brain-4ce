e6b9d68f0bfb37c8207f79aac6887a8c997a9c61
jonathanm-12 <81660305+jonathanm-12@users.noreply.github.com>
2022-12-07 17:24:42 -0500

    control camera pos on 'a'

--- 3D Virtual Environment/hello.py
+++ 3D Virtual Environment/hello.py
@@ -5,31 +5,52 @@ from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import AmbientLight, DirectionalLight, PointLight
 from panda3d.core import NodePath
 from panda3d.core import PandaNode
+from pandac.PandaModules import WindowProperties
+from direct.showbase import DirectObject
 
 
-class MyApp(ShowBase):
+class MyApp(ShowBase, DirectObject.DirectObject):
+
+    xCoord = 0
+    yCoord = 0
+
+    def ChangeCameraPosition(self):
+        self.xCoord = self.xCoord + 5
+        self.yCoord = self.yCoord + 5
+        self.camera.setPos(self.xCoord,self.yCoord,-0.5)
+        print(self.xCoord)
+        print(self.yCoord)
+
+    # def __Mouseinit__(self):
+    #    self.disableMouse()
+
     def __init__(self):
         ShowBase.__init__(self)
+#        self.accept('mouse1', self.__Mouseinit__)
+        self.accept('a', self.ChangeCameraPosition)
 
         blank_node = PandaNode("my_blank_node")
-        blank_node_path = NodePath(blank_node)
-        blank_node_path.reparentTo(self.render)
+        nodepath1 = NodePath(blank_node)
+        nodepath1.reparentTo(self.render)
+
+        blank_node2 = PandaNode("my_blank_node2")
+        nodepath2 = NodePath(blank_node2)
+        nodepath2.reparentTo(self.render)        
 
         dlight = DirectionalLight('my dlight')
 
-
-        plane = self.loader.loadModel("my-objects/plane.egg")
-        #plane.setPos(1,10,1)
-        plane.setScale(10, 10, 10)
-        plane.reparentTo(self.render)
+        self.scene = self.loader.loadModel("my-objects/plane.egg")
+        self.scene.setPos(0,0,-0.5)
+        self.scene.setScale(10, 10, 10)
+        self.scene.reparentTo(nodepath2)
 
         sphObject = self.loader.loadModel("my-objects/sphere.egg")
-        sphObject.setPos(0, 10, 1)
-        sphObject.setScale(1, 1, 1)
-        sphObject.reparentTo(blank_node_path)
+        sphObject.setPos(0, 10, 0.1)
+        sphObject.setScale(.6, .6, .6)
+        sphObject.reparentTo(nodepath1)
 
-        dlnp = blank_node_path.attachNewNode(dlight)
-        blank_node_path.setLight(dlnp)
+        dlnp = nodepath1.attachNewNode(dlight)
+        nodepath1.setLight(dlnp)
 
 
 
@@ -38,3 +59,5 @@ class MyApp(ShowBase):
 game = MyApp()
 
 game.run()
+
+print(game.x)
