3f532960d7d567dbc6271ff780802ed3cfad8cc2
jonathanm-12 <81660305+jonathanm-12@users.noreply.github.com>
2022-12-08 22:02:33 -0500

    barrier, change plane color

--- 3D Virtual Environment/main.py
+++ 3D Virtual Environment/main.py
@@ -5,7 +5,7 @@ from direct.interval.IntervalGlobal import Sequence
 from panda3d.core import AmbientLight, DirectionalLight, PointLight
 from panda3d.core import NodePath
 from panda3d.core import PandaNode
-from pandac.PandaModules import WindowProperties
+from panda3d.core import WindowProperties
 from direct.gui.OnscreenText import OnscreenText
 
 class MyApp(ShowBase):
@@ -20,7 +20,7 @@ class MyApp(ShowBase):
         self.accept('a', self.ChangeCameraPositionLeft)
         self.accept('s', self.ChangeCameraPositionBackward)
         self.accept('w', self.ChangeCameraPositionForward)
-        self.taskMgr.add(self.UpdateCameraPosition)
+        # self.taskMgr.add(self.UpdateCameraPosition)
 
         blank_node = PandaNode("my_blank_node")
         nodepath1 = NodePath(blank_node)
@@ -30,7 +30,9 @@ class MyApp(ShowBase):
         nodepath2 = NodePath(blank_node2)
         nodepath2.reparentTo(self.render)
 
-        dlight = DirectionalLight('my dlight')
+        self.dlight = DirectionalLight('my dlight')
+        self.alight =  AmbientLight('my alight')
+        self.alight.setColor((0.2, 0.2, 0.8, 1))
 
         self.scene = self.loader.loadModel("my-objects/plane.egg")
         self.scene.setPos(0,0,-0.5)
@@ -42,10 +44,21 @@ class MyApp(ShowBase):
         self.sphObject.setScale(.6, .6, .6)
         self.sphObject.reparentTo(nodepath1)
 
+        self.barrier = self.loader.loadModel("my-objects/barrier.egg")
+        self.barrier.setPos(0, 65, 0.1)
+        self.barrier.setScale(6.3, 6.3, 1)
+        self.barrier.setHpr(90,0,0)
+        self.barrier.reparentTo(nodepath1)
+
         self.textObject = OnscreenText(text='x:0 y:0', pos=(-0.5, 0.02), scale=0.07)
 
-        dlnp = nodepath1.attachNewNode(dlight)
+        dlnp = nodepath1.attachNewNode(self.dlight)
         nodepath1.setLight(dlnp)
+
+        alnp = nodepath2.attachNewNode(self.alight)
+        nodepath2.setLight(alnp)
+
+        print(self.barrier.getBounds())
 
 
     def ChangeCameraPositionForward(self):
@@ -60,12 +73,12 @@ class MyApp(ShowBase):
     def ChangeCameraPositionLeft(self):
         self.xCoord -= 5           
 
-    def UpdateCameraPosition(self, task):
-        self.textObject.destroy()
-        self.textObject = OnscreenText(text='x: ' + str(self.xCoord) + ' y:' + str(self.yCoord), pos=(-0.5, 0.02), scale=0.07)
-        self.camera.setPos(self.xCoord, self.yCoord, 0)
-        self.sphObject.setPos(self.xCoord,10+self.yCoord, 0.1)
-        return task.cont
+    # def UpdateCameraPosition(self, task):
+    #     self.textObject.destroy()
+    #     self.textObject = OnscreenText(text='x: ' + str(self.xCoord) + ' y:' + str(self.yCoord), pos=(-0.5, 0.02), scale=0.07)
+    #     self.camera.setPos(self.xCoord, self.yCoord, 0)
+    #     self.sphObject.setPos(self.xCoord,10+self.yCoord, 0.1)
+    #     return task.cont
 
 
 
