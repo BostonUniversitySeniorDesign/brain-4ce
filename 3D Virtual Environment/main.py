dd6a458af660244f4a2dc912e41c6ce2f362c57b
jonathanm-12 <81660305+jonathanm-12@users.noreply.github.com>
2023-02-01 16:20:24 -0500

    tentative camera angle changes

--- 3D Virtual Environment/main.py
+++ 3D Virtual Environment/main.py
@@ -7,14 +7,19 @@ from panda3d.core import NodePath
 from panda3d.core import PandaNode
 from panda3d.core import WindowProperties
 from direct.gui.OnscreenText import OnscreenText
+import math
 
 class MyApp(ShowBase):
     xCoord = 0
     yCoord = 0
+    angle  = 0
     textObject = None
 
     def __init__(self):
         ShowBase.__init__(self)
+        # ShowBase.useDrive(self)
+        # ShowBase.useTrackball(self)
+        ShowBase.oobe(self)
 
         self.accept('d', self.ChangeCameraPositionRight)
         self.accept('a', self.ChangeCameraPositionLeft)
@@ -42,7 +47,7 @@ class MyApp(ShowBase):
         self.sphObject = self.loader.loadModel("my-objects/sphere.egg")
         self.sphObject.setPos(0, 10, 0.1)
         self.sphObject.setScale(.6, .6, .6)
-        self.sphObject.reparentTo(nodepath1)
+        self.sphObject.reparentTo(self.render)
 
         self.barrier = self.loader.loadModel("my-objects/barrier.egg")
         self.barrier.setPos(0, 300, 1.63)
@@ -50,13 +55,39 @@ class MyApp(ShowBase):
         self.barrier.setHpr(90,0,0)
         self.barrier.reparentTo(nodepath1)
 
+
+        self.barrier2 = self.loader.loadModel("my-objects/barrier.egg")
+        self.barrier2.setPos(300, 0, 1.63)
+        self.barrier2.setScale(6.3, 31.3, 1)
+        self.barrier2.setHpr(180,0,0)
+        self.barrier2.reparentTo(nodepath1)
+
+
+        self.barrier3 = self.loader.loadModel("my-objects/barrier.egg")
+        self.barrier3.setPos(-300, 0, 1.63)
+        self.barrier3.setScale(6.3, 31.3, 1)
+        self.barrier3.setHpr(180,0,0)
+        self.barrier3.reparentTo(nodepath1)        
+
+
+        self.barrier4 = self.loader.loadModel("my-objects/barrier.egg")
+        self.barrier4.setPos(0, -300, 1.63)
+        self.barrier4.setScale(6.3, 31.3, 1)
+        self.barrier4.setHpr(90,0,0)
+        self.barrier4.reparentTo(nodepath1)        
+
+
+
+
         self.textObject = OnscreenText(text='x:0 y:0', pos=(-0.5, 0.02), scale=0.07)
 
         dlnp = nodepath1.attachNewNode(self.dlight)
-        nodepath1.setLight(dlnp)
+
+        self.render.setLight(dlnp)
 
         alnp = nodepath2.attachNewNode(self.alight)
         nodepath2.setLight(alnp)
+
 
         print(self.barrier.getBounds())
 
@@ -68,17 +99,23 @@ class MyApp(ShowBase):
         self.yCoord -= 5     
 
     def ChangeCameraPositionRight(self):
-        self.xCoord += 5          
+        self.xCoord += 5 * math.cos(math.radians(self.angle))
+        self.yCoord += 5 * math.sin(math.radians(self.angle))
+        self.angle += 90
 
     def ChangeCameraPositionLeft(self):
-        self.xCoord -= 5           
+        self.xCoord -= 5 * math.cos(math.radians(self.angle))
+        self.yCoord -= 5 * math.sin(math.radians(self.angle))
+        self.angle -= 90
 
     def UpdateCameraPosition(self, task):
         self.textObject.destroy()
         self.textObject = OnscreenText(text='x: ' + str(self.xCoord) + ' y:' + str(self.yCoord), pos=(-0.5, 0.02), scale=0.07)
         self.camera.setPos(self.xCoord, self.yCoord, 0)
-        self.sphObject.setPos(self.xCoord,10+self.yCoord, 0.1)
+#        self.camera.setHpr(self.angle, 0, 0)
+        self.sphObject.setPos(self.xCoord, 10 + self.yCoord, 0.1)
         return task.cont
+
 
 
 
