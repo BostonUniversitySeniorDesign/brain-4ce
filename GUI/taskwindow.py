ce4cef5d50ae51ac312d0e2155d13e6ba08529b7
jonathanm-12 <mikalovjonathan@gmail.com>
2023-03-27 16:28:55 -0400

    color loop WIP

--- GUI/taskwindow.py
+++ GUI/taskwindow.py
@@ -1,16 +1,26 @@
 import tkinter as tk
 import time
 
-# def changecirc(canvas):
-#     i =  0
-#     for i in range(6):
-#         canvas.itemconfig(i, fill='yellow')
-#         if i > 0 and i != 5:
-#             canvas.itemconfig(i-1, fill='gray')
-#         if i == 6:
-#             i = -1
-        
-#         i  = i + 1
+class Circle:
+    def __init__(self, canvas, x, y):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.radius = 50
+        self.color_index = 0
+        self.colors = ["red", "white"]
+        self.draw()
+
+    def draw(self):
+        self.canvas.delete("all")
+        for i in range(1):
+            color = self.colors[(self.color_index+i)%len(self.colors)]
+            #x = self.x + i * 120 - 60
+            self.canvas.create_oval(self.x, self.y, self.x+50, self.y+50, fill=color)
+        self.color_index = (self.color_index + 1) % len(self.colors)
+        self.canvas.scale("all", 0, 0, 2, 2)
+        self.canvas.after(500, self.draw)
+
 
 def tWindow():
 
@@ -31,26 +41,29 @@ def tWindow():
     canvas = tk.Canvas(window, width=450, height=450)
     canvas.pack()
 
-    ovals = []
+    # ovals = []
 
-    ovals.append(canvas.create_oval(25, 25, 75, 75, outline='black', width=2))
-    ovals.append(canvas.create_oval(100, 25, 150, 75, outline='black', width=2))
-    ovals.append(canvas.create_oval(175, 25, 225, 75, outline='black', width=2))
-    ovals.append(canvas.create_oval(25, 100, 75, 150, outline='black', width=2))
-    ovals.append(canvas.create_oval(100, 100, 150, 150, outline='black', width=2))
-    ovals.append(canvas.create_oval(175, 100, 225, 150, outline='black', width=2))
+    # ovals.append(canvas.create_oval(25, 25, 75, 75, outline='black', width=2))
+    # ovals.append(canvas.create_oval(100, 25, 150, 75, outline='black', width=2))
+    # ovals.append(canvas.create_oval(175, 25, 225, 75, outline='black', width=2))
+    # ovals.append(canvas.create_oval(25, 100, 75, 150, outline='black', width=2))
+    # ovals.append(canvas.create_oval(100, 100, 150, 150, outline='black', width=2))
+    # ovals.append(canvas.create_oval(175, 100, 225, 150, outline='black', width=2))
 
-
+    circle1 = Circle(canvas, 25, 75)
+    # circle2 = Circle(canvas, 100, 25)
+    # circle3 = Circle(canvas, 0, 0)
+    # circle4 = Circle(canvas, 25, 100)
+    # circle5 = Circle(canvas, 100, 100)
+    # circle6 = Circle(canvas, 175, 100)
 
     canvas.scale("all", 0, 0, 2, 2)
-    canvas.place(relx=0.45, rely=0.05, anchor='n')
-    canvas.create_text(100, 100, text="1", fill="black", font=('Helvetica 25 bold'))
-    canvas.create_text(250, 100, text="2", fill="black", font=('Helvetica 25 bold'))   
-    canvas.create_text(400, 100, text="3", fill="black", font=('Helvetica 25 bold'))
-    canvas.create_text(100, 250, text="4", fill="black", font=('Helvetica 25 bold'))
-    canvas.create_text(250, 250, text="5", fill="black", font=('Helvetica 25 bold'))
-    canvas.create_text(400, 250, text="6", fill="black", font=('Helvetica 25 bold'))        
-    
-
+    # canvas.place(relx=0.45, rely=0.05, anchor='n')
+    # canvas.create_text(100, 100, text="1", fill="black", font=('Helvetica 25 bold'))
+    # canvas.create_text(250, 100, text="2", fill="black", font=('Helvetica 25 bold'))   
+    # canvas.create_text(400, 100, text="3", fill="black", font=('Helvetica 25 bold'))
+    # canvas.create_text(100, 250, text="4", fill="black", font=('Helvetica 25 bold'))
+    # canvas.create_text(250, 250, text="5", fill="black", font=('Helvetica 25 bold'))
+    # canvas.create_text(400, 250, text="6", fill="black", font=('Helvetica 25 bold'))            
 
     window.mainloop()
