ee7d87126336ea05d06d4bc4fa073c651e83fb60
jonathanm-12 <mikalovjonathan@gmail.com>
2023-04-02 17:08:18 -0400

    loop colors between tasks

--- GUI/taskwindow.py
+++ GUI/taskwindow.py
@@ -1,18 +1,21 @@
 import tkinter as tk
-import time
 
 class Circle:
-    def __init__(self, canvas):
+    def __init__(self, canvas, window):
+        self.window = window
         self.canvas = canvas
-        self.draw() 
-
+        self.iswhite = True
+        self.curr = 0
+        self.draw()
+        
     def draw(self):
         self.canvas.delete("all")
         self.x = 45
         self.y = 25
+        self.circles = []
         for i in range(6):
-            
-            self.canvas.create_oval(self.x, self.y, self.x+50, self.y+50, fill='white')
+            circle = self.canvas.create_oval(self.x, self.y, self.x+50, self.y+50, fill='white')
+            self.circles.append(circle)
             self.x += 75
             
             if i == 2:
@@ -20,11 +23,26 @@ class Circle:
                 self.x = 45
 
         self.canvas.scale("all", 0, 0, 2, 2)
-        self.canvas.after(500, self.draw)
+
+        if self.iswhite == True:
+            self.change_color()
+            self.iswhite = False
+        else:
+            self.change_color()
+            self.iswhite = True
+
+        self.canvas.after(1000, self.draw)
+
+
+        
+    def change_color(self):
+        self.canvas.itemconfig(self.circles[self.curr], fill='red')
+        self.curr = self.curr + 1
+        if self.curr == 6:
+            self.curr = 0
 
 
 def tWindow():
-
     window = tk.Tk()
     window.title("Brain4ce Menu")
 
@@ -42,12 +60,9 @@ def tWindow():
     canvas = tk.Canvas(window, width=600, height=400)
     canvas.pack()
 
-
     canvas.scale("all", 0, 0, 1.5, 1.5)
-    Circle(canvas)
-           
+    Circle(canvas, window)
 
     window.mainloop()
-
 
 tWindow()
