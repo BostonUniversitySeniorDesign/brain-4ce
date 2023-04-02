849b563d2fb1e28eb00fa7a072ef6b1165d29679
jonathanm-12 <mikalovjonathan@gmail.com>
2023-04-02 16:05:43 -0400

    fix code organization, remove redundancies

--- GUI/main.py
+++ GUI/main.py
@@ -1,45 +1,50 @@
 import tkinter as tk
 import taskwindow
 
-def StartImagery():
-    print("bImg selected")
 
-def StartReal():
-    window.destroy()
-    print("real selected")
-    taskwindow.tWindow()
+class MainGUI:
+    def StartImagery(self):
+        print("bImg selected")
+
+    def StartReal(self):
+        self.window.destroy()
+        print("real selected")
+        taskwindow.tWindow()
+
+    def __init__(self):
+
+        self.window = tk.Tk()
+        self.window.title("Brain4ce Menu")
+
+        #Set window size and pos
+
+        self.screen_width = self.window.winfo_screenwidth()
+        self.screen_height = self.window.winfo_screenheight()
+        self.window_width = 300
+        self.window_height = 200
+        self.x_pos = (self.screen_width - self.window_width) // 2
+        self.y_pos = (self.screen_height - self.window_height) // 2
+
+        self.window.geometry("{}x{}+{}+{}".format(self.window_width, self.window_height, self.x_pos, self.y_pos))
+
+        users = ['User 1', 'User 2', 'User 3', 'User 4', 'User 5']
+        variable = tk.StringVar(self.window)
+        variable.set(users[0])
+
+        dUsers = tk.OptionMenu(self.window, variable, *users)
+        dUsers.configure(background="white", activebackground="white")
+        dUsers["menu"].configure(bg="white")
+        #dUsers.pack()
+        dUsers.place(relx=0.5, rely=0.05, anchor='n')
+
+        bStartImg = tk.Button(self.window, text="Start Imagery", command=self.StartImagery)
+        bStartImg.place(relx=0.4, rely=0.5, anchor="se")
+
+        bStartReal = tk.Button(self.window, text="Start Real", command=self.StartReal)
+        bStartReal.place(relx=0.8, rely=0.5, anchor = "se")
+
+        self.window.mainloop()
 
 
-window = tk.Tk()
-window.title("Brain4ce Menu")
+main = MainGUI()
 
-#Set window size and pos
-
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-window_width = 300
-window_height = 200
-x_pos = (screen_width - window_width) // 2
-y_pos = (screen_height - window_height) // 2
-
-window.geometry("{}x{}+{}+{}".format(window_width, window_height, x_pos, y_pos))
-
-users = ['User 1', 'User 2', 'User 3', 'User 4', 'User 5']
-variable = tk.StringVar(window)
-variable.set(users[0])
-
-dUsers = tk.OptionMenu(window, variable, *users)
-dUsers.configure(background="white", activebackground="white")
-dUsers["menu"].configure(bg="white")
-#dUsers.pack()
-dUsers.place(relx=0.5, rely=0.05, anchor='n')
-
-bStartImg = tk.Button(window, text="Start Imagery", command=StartImagery)
-bStartImg.place(relx=0.4, rely=0.5, anchor="se")
-
-bStartReal = tk.Button(window, text="Start Real", command=StartReal)
-bStartReal.place(relx=0.8, rely=0.5, anchor = "se")
-
-
-
-window.mainloop()
--- GUI/taskwindow.py
+++ GUI/taskwindow.py
@@ -2,22 +2,23 @@ import tkinter as tk
 import time
 
 class Circle:
-    def __init__(self, canvas, x, y):
+    def __init__(self, canvas):
         self.canvas = canvas
-        self.x = x
-        self.y = y
-        self.radius = 50
-        self.color_index = 0
-        self.colors = ["red", "white"]
-        self.draw()
+        self.draw() 
 
     def draw(self):
         self.canvas.delete("all")
-        for i in range(1):
-            color = self.colors[(self.color_index+i)%len(self.colors)]
-            #x = self.x + i * 120 - 60
-            self.canvas.create_oval(self.x, self.y, self.x+50, self.y+50, fill=color)
-        self.color_index = (self.color_index + 1) % len(self.colors)
+        self.x = 45
+        self.y = 25
+        for i in range(6):
+            
+            self.canvas.create_oval(self.x, self.y, self.x+50, self.y+50, fill='white')
+            self.x += 75
+            
+            if i == 2:
+                self.y += 75
+                self.x = 45
+
         self.canvas.scale("all", 0, 0, 2, 2)
         self.canvas.after(500, self.draw)
 
@@ -38,32 +39,15 @@ def tWindow():
 
     window.geometry("{}x{}+{}+{}".format(window_width, window_height, x_pos, y_pos))
 
-    canvas = tk.Canvas(window, width=450, height=450)
+    canvas = tk.Canvas(window, width=600, height=400)
     canvas.pack()
 
-    # ovals = []
 
-    # ovals.append(canvas.create_oval(25, 25, 75, 75, outline='black', width=2))
-    # ovals.append(canvas.create_oval(100, 25, 150, 75, outline='black', width=2))
-    # ovals.append(canvas.create_oval(175, 25, 225, 75, outline='black', width=2))
-    # ovals.append(canvas.create_oval(25, 100, 75, 150, outline='black', width=2))
-    # ovals.append(canvas.create_oval(100, 100, 150, 150, outline='black', width=2))
-    # ovals.append(canvas.create_oval(175, 100, 225, 150, outline='black', width=2))
-
-    circle1 = Circle(canvas, 25, 75)
-    # circle2 = Circle(canvas, 100, 25)
-    # circle3 = Circle(canvas, 0, 0)
-    # circle4 = Circle(canvas, 25, 100)
-    # circle5 = Circle(canvas, 100, 100)
-    # circle6 = Circle(canvas, 175, 100)
-
-    canvas.scale("all", 0, 0, 2, 2)
-    # canvas.place(relx=0.45, rely=0.05, anchor='n')
-    # canvas.create_text(100, 100, text="1", fill="black", font=('Helvetica 25 bold'))
-    # canvas.create_text(250, 100, text="2", fill="black", font=('Helvetica 25 bold'))   
-    # canvas.create_text(400, 100, text="3", fill="black", font=('Helvetica 25 bold'))
-    # canvas.create_text(100, 250, text="4", fill="black", font=('Helvetica 25 bold'))
-    # canvas.create_text(250, 250, text="5", fill="black", font=('Helvetica 25 bold'))
-    # canvas.create_text(400, 250, text="6", fill="black", font=('Helvetica 25 bold'))            
+    canvas.scale("all", 0, 0, 1.5, 1.5)
+    Circle(canvas)
+           
 
     window.mainloop()
+
+
+tWindow()
