7fd8292b87582fbf8e3646962bb9dbe3b6011af6
jonathanm-12 <81660305+jonathanm-12@users.noreply.github.com>
2023-04-27 22:27:42 -0400

    add countdown, final store, fix bug

--- 30s Game/main.py
+++ 30s Game/main.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+from tkinter import Tk, Label
 
 # Initialize Pygame
 pygame.init()
@@ -34,23 +35,55 @@ timer_started = False
 
 box_start_time = None
 box_timer_started = False
-
+rand_num = height
 box_bottom = height/2 - 50
 box_top = height/2 - 60 + 0.1*height
 
 
 font = pygame.font.Font(None, 36)
 
-time_text = "Time: 0.0s"
+time_text = "Score: 0"
 text = font.render(time_text, True, black)
 elapsed_time = 0
 box_elapsed_time = 0
 total = 0
+score = 0
+
+countdown_font = pygame.font.Font(None, 36)
+countdown_time = 30
+countdown_text = "Time: {:.0f}s".format(countdown_time)
+countdown = countdown_font.render(countdown_text, True, black)
+
+countdown_start = time.time()
+
+pygame.draw.rect(screen, 'green', (width/2 - 25, rand_num/2-50, player_size, 0.1*height))
+pygame.draw.rect(screen, black, (player_x, player_y, player_size, player_size/2.5))
+pygame.display.update()
+
+def gen_rand():
+    rand = random.uniform(0, height*2-50)
+    if rand > (height*2-50)/2:
+        rand += 50
+    else:
+        rand -= 50
+    return int(rand)
+
 
 while True:
 
     screen.fill('yellow')
 
+    if time.time() - countdown_start >= 1:
+        countdown_time -= 1
+        countdown_text = "Time: {}s".format(countdown_time)
+        countdown = countdown_font.render(countdown_text, True, black)
+        countdown_start = time.time()
+        if countdown_time == 0:
+            break
+
+
+    # Display the countdown timer
+    screen.blit(countdown, (380, 10))    
     
     # checks if the user tried to quit the window
     if len(pygame.event.get(eventtype=pygame.QUIT)):
@@ -72,7 +105,10 @@ while True:
             timer_started = True
         
         elapsed_time = time.time() - start_time + total
-        time_text = "Time: {:.1f}s".format(elapsed_time)
+
+        score = int(elapsed_time / 0.5)     
+
+        time_text = "Score: {:.0f}".format(score)
         text = font.render(time_text, True, black)
         screen.blit(text, (10, 10))
     else:
@@ -81,29 +117,46 @@ while True:
 
     # Draw the screen
     pygame.draw.rect(screen, red, (width/2 - 25, 0, player_size, height))
-    #pygame.draw.rect(screen, 'green', (width/2 - 25, height/2-50, player_size, 0.1*height))
     
+
 
     if not box_timer_started:
         box_start_time = time.time()
 
         box_timer_started = True
     
-    if box_elapsed_time >= 1:
-        rand_num = random.randint(0, height*2-10)
+    if box_elapsed_time >= 5:
+        rand_num = gen_rand()
+        
         box_start_time = time.time()
         box_elapsed_time = 0
-        print("1s")
-        pygame.draw.rect(screen, 'green', (width/2 - 25, rand_num/2-50, player_size, 0.1*height))
-        box_top = rand_num/2 - 50
-        box_bottom = box_top - 10 + 0.1*height
-        print(box_top, box_bottom)
-        pygame.display.update()
+        box_top = rand_num/2 - 60 + 0.1*height
+        box_bottom = rand_num/2 - 50
 
     else:
         box_elapsed_time = time.time() - box_start_time
-    
-#    pygame.draw.rect(screen, black, (player_x, player_y, player_size, player_size/2.5))
-    #pygame.display.update()
+
+    pygame.draw.rect(screen, 'green', (width/2 - 25, rand_num/2-50, player_size, 0.1*height))
+    pygame.draw.rect(screen, black, (player_x, player_y, player_size, player_size/2.5))
+    pygame.display.update()
+
+    pygame.display.update()
 
 pygame.quit()
+
+root = Tk()
+root.title("Game Over")
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+window_width = 200
+window_height = 100
+x_pos = (screen_width - window_width) // 2
+y_pos = (screen_height - window_height) // 2
+root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_pos, y_pos))
+label = Label(root, text="You finished!")
+label.pack()
+
+label2 = Label(root, text="Your final score is " + str(score))
+label2.pack()
+
+root.mainloop()    
